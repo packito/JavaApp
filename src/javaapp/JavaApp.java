285a1a8b412c8ceb3cdb2c3ab649e1155003294b
packito <cesik.tonda@seznam.cz>
2012-01-15 12:24:33 +0100

    da commit

---
+++ src/javaapp/JavaApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+import java.awt.Color;
+
+
+/**
+ *
+ * @author cesa
+ */
+public class JavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("herp derp");
+    }
+
+
+}
